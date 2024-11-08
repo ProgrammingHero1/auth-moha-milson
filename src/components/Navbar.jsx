b3702df30e8e62f5ecfcf39f963aefb789939d40
Programming Hero <programming.hero1@gmail.com>
2024-11-08 06:32:44 -0600

    create auth context and auth provider to share auth info

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 
 const Navbar = () => {
+
+    const { name } = useContext(AuthContext);
+
+    console.log(name)
+
     const links = <>
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/login">Login</NavLink></li>
         <li><NavLink to="/register">Register</NavLink></li>
-        
+
     </>
     return (
         <div className="navbar bg-base-100">
@@ -40,7 +46,7 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a className="btn">{name}</a>
             </div>
         </div>
     );
